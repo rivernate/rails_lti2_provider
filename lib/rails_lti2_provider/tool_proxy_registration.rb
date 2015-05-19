dcb3120d0c12d096727b4412789c178d24e619d9
Nathan Mills <nathanm@instructure.com>
2015-05-19 15:35:03 -0600

    fix broken route helper method call

--- lib/rails_lti2_provider/tool_proxy_registration.rb
+++ lib/rails_lti2_provider/tool_proxy_registration.rb
@@ -61,7 +61,7 @@ module RailsLti2Provider
       registered_proxy = registration_service.register_tool_proxy(tool_proxy)
       if registered_proxy
         tool_proxy.tool_proxy_guid = registered_proxy.tool_proxy_guid
-        tool_proxy.id = controller.send(engine_name).show_tool_proxy_url(registered_proxy.tool_proxy_guid)
+        tool_proxy.id = controller.send(engine_name).show_tool_url(registered_proxy.tool_proxy_guid)
         tp = Tool.create!(shared_secret: tool_proxy.security_contract.shared_secret, uuid: registered_proxy.tool_proxy_guid, tool_settings: tool_proxy.as_json, lti_version: tool_proxy.lti_version)
         registration.update(workflow_state: 'registered', tools: tp)
         {
