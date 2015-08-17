bd59973655e7765d2690c117e15546093c9bb3fc
Nathan Mills <nathanm@instructure.com>
2015-08-17 13:44:06 -0500

    remove shared secret limit

--- db/migrate/20150817183400_update_tool_proxy_shared_secret.rb
+++ db/migrate/20150817183400_update_tool_proxy_shared_secret.rb
@@ -1,5 +1,5 @@
 class UpdateToolProxySharedSecret < ActiveRecord::Migration
   def change
-    change_column :rails_lti2_provider_tools, :shared_secret, :text
+    change_column :rails_lti2_provider_tools, :shared_secret, :text, limit: nil
   end
 end
