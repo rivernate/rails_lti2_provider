51dd99cac21ece7d3a1f76f476ba2b4fa0c96853
Nathan Mills <nathanm@instructure.com>
2016-02-19 16:47:04 -0700

    fix migration for postgres

--- db/migrate/20151203231519_update_tool_id_type.rb
+++ db/migrate/20151203231519_update_tool_id_type.rb
@@ -1,8 +1,8 @@
 class UpdateToolIdType < ActiveRecord::Migration
   def change
 
-    change_column :rails_lti2_provider_lti_launches, :tool_id, :integer, limit: 8
-    change_column :rails_lti2_provider_registrations, :tool_id, :integer, limit: 8
+    change_column :rails_lti2_provider_lti_launches, :tool_id, "bigint USING CAST(tool_id AS bigint)"
+    change_column :rails_lti2_provider_registrations, :tool_id,"bigint USING CAST(tool_id AS bigint)"
 
   end
 end
